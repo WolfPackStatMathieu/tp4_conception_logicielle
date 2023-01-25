849057e53fd97aaa6b20de069e0a37dd214de930
Antoine Brunetti <antoine.brunetti@insee.fr>
2023-01-25 02:08:43 +0100

    update

--- application/model/interpreteur_impl/no_parentheses_interpreteur.py
+++ application/model/interpreteur_impl/no_parentheses_interpreteur.py
@@ -1,13 +1,15 @@
+from application.model.calculette import Calculette
 from application.model.interpreteur import Interpreteur
 
 
 class NoParenthesesInterpreteur(Interpreteur):
     def calculer_from_expression(self, expression: str):
-        if self._contains_invalid_characters(expression=expression):
+        without_spaces_expression = expression.replace(" ", "")
+        if self._contains_invalid_characters(expression=without_spaces_expression):
             raise ValueError("invalid characters found in expression")
-        if self._has_invalid_syntax(expression=expression):
+        if self._has_invalid_syntax(expression=without_spaces_expression):
             raise ValueError("invalid ordering of values in expression")
-
+        calcul = self.compute(self, expression)
         return calcul
 
     def _contains_invalid_characters(self, expression: str):
@@ -25,10 +27,38 @@ class NoParenthesesInterpreteur(Interpreteur):
         nb_caracteres_matches = len("".join(res))
         return (nb_caracteres_matches + 1) != len(expression.replace(" ", ""))
 
-    def compute(self, expression: str, temporary_result: int = 0) -> int:
-        trimmed_expression = expression.replace(" ", "")
-        while len(expression) > 0:
-            computed_expression = trimmed_expression.slice(0, 1)
+    def compute(self, expression: str) -> int:
+        if "*" in expression or "/" in expression:
+            index = expression.index("*")
+            operation = self.__effectuer_operation(
+                expression[index - 1], expression[index], expression[index + 1]
+            )
+            new_expression = (
+                expression[: index - 1 :] + operation + expression[index + 1 : :]
+            )
+            self.compute(new_expression)
+        operation = self.__effectuer_operation(
+            expression[0], expression[1], expression[2]
+        )
+        new_expression = operation + expression[index + 1 : :]
+        try:
+            self.compute(new_expression)
+        except:
+            return int(expression)
 
-            compute()
-        return temporary_result
+    def __effectuer_operation(self, number1: str, operator: str, number2: str):
+        function = self.get_function(operator)
+        result = function(number1, number2)
+        return str(result)
+
+    def get_function(self, char: str) -> callable:
+        if char == "+":
+            return lambda a, b: Calculette.somme(a, b)
+        if char == "-":
+            return lambda a, b: Calculette.soustraction(a, b)
+        if char == "*":
+            return lambda a, b: Calculette.produit(a, b)
+        if char == "/":
+            return lambda a, b: Calculette.division(a, b)
+        else:
+            raise NotImplementedError("operator not implemented")
